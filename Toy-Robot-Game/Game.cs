6111d298cf30a9e9d8e2eb5b6cb0e53bfeee8b6f
Leonardql <31765989+Leonardql@users.noreply.github.com>
2023-05-08 12:55:48 +0200

    Added Game Class with Interface ICommand
    properties, constructor and methods: PlaceRobot,PlaceWall,Report,Move,Right,Left

---
+++ Toy-Robot-Game/Game.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toy_Robot_Game
+{
+    public class Game : ICommand
+    {
+        Board board;
+        Robot robot;
+
+        public Game(Board board, Robot robot)
+        {
+            this.board = board;
+            this.robot = robot;
+        }
+
+        
+
+        
+        public void PlaceRobot(int row, int col, string facing)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PlaceWall(int row, int col)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Report()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public void Move()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public void Right()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Left()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
