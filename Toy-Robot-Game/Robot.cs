95bbf100c88fbc380d76c40db744e1f190719e36
Leonardql <31765989+Leonardql@users.noreply.github.com>
2023-05-08 12:46:16 +0200

    Added Robot Class
    properties, constructor and a check if facing is valid

---
+++ Toy-Robot-Game/Robot.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toy_Robot_Game
+{
+    public class Robot
+    {
+        private int col;
+        private string facing;
+        private int row;
+
+        public Robot()
+        {
+        }
+
+        public int Col { get => col; set => col = value; }
+        public int Row { get => row; set => row = value; }
+        public string Facing { get => facing; set => facing = value; }
+
+        public bool FacingIsValid(string nfacing)
+        {
+            if (nfacing == "NORTH" || nfacing == "SOUTH" || nfacing == "WEST" || nfacing == "EAST")
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
