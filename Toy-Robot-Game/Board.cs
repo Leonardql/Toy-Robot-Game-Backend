26c2d305fa03f7a386c39a61224a82fa74284a1e
Leonardql <31765989+Leonardql@users.noreply.github.com>
2023-05-08 12:50:55 +0200

    Added Board Class
    properties, constructor

---
+++ Toy-Robot-Game/Board.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toy_Robot_Game
+{
+    public class Board
+    {
+        private int rows;
+        private int cols;
+        private int[,] board;
+
+        public Board(int rows, int cols)
+        {
+            this.rows = rows;
+            this.cols = cols;
+            board = new int[rows + 1, cols + 1];
+        }
+
+
+    }
+}
