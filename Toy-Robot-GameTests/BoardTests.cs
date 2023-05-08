342e6a2b826bff3c9f29aa93a4602d8993faf7cc
Leonardql <31765989+Leonardql@users.noreply.github.com>
2023-05-08 14:30:50 +0200

    Added tests for Board, Robot
    Should have done this before tbh

---
+++ Toy-Robot-GameTests/BoardTests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Toy_Robot_Game;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toy_Robot_Game.Tests
+{
+    [TestClass()]
+    public class BoardTests
+    {
+        [TestMethod()]
+        public void IsValidLocationTest()
+        {
+            Board board = new Board(5, 5);
+
+
+            Assert.AreEqual(board.IsValidLocation(5, 6), false);
+            Assert.AreEqual(board.IsValidLocation(1, 2), true);
+
+        }
+
+        [TestMethod()]
+        public void IsEmptySquareTest()
+        {
+
+            Board board = new Board(5, 5);
+
+            board.PlaceOnBoard(1, 2, 1);
+
+            Assert.AreEqual(board.IsEmptySquare(1, 2), false);
+
+            board.PlaceOnBoard(1, 2, 0);
+
+            Assert.AreEqual(board.IsEmptySquare(1, 2), true);
+        }
+
+        
+
+        [TestMethod()]
+        public void PlaceOnBoardTest()
+        {
+            Board board = new Board(5, 5);
+
+            board.PlaceOnBoard(1, 2, 1);
+
+            Assert.AreEqual(board.Square(1, 2), 1);
+
+
+        }
+    }
+}
---
+++ Toy-Robot-GameTests/RobotTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Toy_Robot_Game;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toy_Robot_Game.Tests
+{
+    [TestClass()]
+    public class RobotTests
+    {
+        [TestMethod()]
+        public void IsFacingValidTest()
+        {
+            Robot robot = new Robot();
+
+            Assert.AreEqual(robot.IsFacingValid("CENTER"), false);
+
+            Assert.AreEqual(robot.IsFacingValid("NORTH"), true);
+        }
+    }
+}
